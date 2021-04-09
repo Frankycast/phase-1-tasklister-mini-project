3e24eae50b76fbc7e2f11aeaf354687696211ec0
Francisco Castillo <Francisco21castillo@aol.com>
2021-04-09 15:36:59 -0400

    almost done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+let taskButton = document.querySelector('#create-task-form')
+
+let toDoUl= document.querySelector('#tasks')
+
+let blankLi= document.createElement("li")
+
+taskButton.addEventListener("submit", (evt) =>{
+  evt.preventDefault()
+  let form = evt.target
+  let blankLi= document.createElement("li")
+  //blankLi.className= `toDoList`
+  blankLi.innerText= `testing`
+  toDoUl.append(blankLi)
+})
